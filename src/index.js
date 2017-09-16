5daa86036d6a98f8abe575cbb789469d5194cbae
benjamindehli <superelg@gmail.com>
2017-09-16 16:04:22 +0200

    Add local stylesheet

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import * as quark from 'quark-gui';
 
 import * as homePage from "./modules/pages/home";
 
+require("../src/style/main.scss");
+
+
 quark.Init.default();
 
 document.body.innerHTML = homePage.page;
